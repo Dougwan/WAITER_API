1f40507a8d0bdef950a6cdb359a2970b52314c27
Douglas M.M Louteiro <douglas.louterio@gebit.io>
2022-11-15 08:49:10 -0300

    feat: created category and product useCases

--- src/router.ts
+++ src/router.ts
@@ -1,30 +1,37 @@
 import { Router } from "express";
+import multer from "multer";
+import path from "node:path";
+import crypto from "crypto";
+
+import { createCategory } from "./app/useCases/categories/createCategory";
+import { listCategories } from "./app/useCases/categories/listCategories";
+import { createProduct } from "./app/useCases/products/createProduct";
+import { listProducts } from "./app/useCases/products/listProducts";
 
 export const router = Router();
 
-// LIST CATEGORIES
-router.get("/categories", (req, res) => {});
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, "..", "uploads"));
+    },
+    filename(req, file, callback) {
+      callback(null, `${crypto.randomUUID()}.${file.mimetype.split("/")[1]}`);
+    },
+  }),
+});
 
-// CREATE CATEGORY
-router.post("/categories", (req, res) => {});
+// CATEGORIES
+router.get("/categories", listCategories);
+router.post("/categories", createCategory);
 
-// LIST PRODUCTS
-router.get("/products", (req, res) => {});
-
-// CREATE PRODUCT
-router.post("/products", (req, res) => {});
-
-// GET PRODUCTS BY CATEGORY
+// PRODUCTS
+router.get("/products", listProducts);
+router.post("/products", upload.single("image"), createProduct);
 router.get("/categories/:categoryId/products", (req, res) => {});
 
-// LIST ORDERS
+// ORDERS
 router.get("/orders", (req, res) => {});
-
-// CREATE ORDER
 router.post("/orders", (req, res) => {});
-
-// CHANGE ORDER STATUS
 router.patch("/orders/:orderId", (req, res) => {});
-
-// DELETE/CANCEL ORDER
 router.delete("/orders/:orderId", (req, res) => {});
